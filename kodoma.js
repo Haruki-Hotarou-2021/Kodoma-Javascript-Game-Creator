a19017b1ee87260031e413b9abc4d68fa7807cbb
Haruki-Hotarou-2021 <hotarouharuki@gmail.com>
2024-04-22 19:57:29 -0300

    update

--- kodoma.js
+++ kodoma.js
@@ -43,3 +43,62 @@ class spr {
     ctx.drawImage(this.sprite, this.x, this.y, this.width, this.height);
   }
 }
+
+class btn {
+  constructor(type, width, height, x, y, color, fill) {
+    this.type = type;
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.fill = fill;
+    this.display();
+  }
+
+  display() {
+    switch (this.type) {
+      case "circ":
+        this._desenharCirculo(ctx);
+        break;
+      case "quad":
+        this._desenharQuadradoArredondado(ctx);
+        break;
+    }
+  }
+
+  _desenharCirculo(ctx) {
+    ctx.beginPath();
+    ctx.arc(this.x + this.width / 2, this.y + this.height / 2, this.width / 2, 0, 2 * Math.PI);
+
+    if (this.fill) {
+      ctx.fillStyle = this.color;
+      ctx.fill();
+    } else {
+      ctx.strokeStyle = this.color;
+      ctx.stroke();
+    }
+  }
+
+  _desenharQuadradoArredondado(ctx) {
+    const radius = 5; // Raio de arredondamento das bordas
+    ctx.beginPath();
+    ctx.moveTo(this.x + radius, this.y);
+    ctx.lineTo(this.x + this.width - radius, this.y);
+    ctx.quadraticCurveTo(this.x + this.width, this.y, this.x + this.width, this.y + radius);
+    ctx.lineTo(this.x + this.width, this.y + this.height - radius);
+    ctx.quadraticCurveTo(this.x + this.width - radius, this.y + this.height, this.x + this.x - radius, this.y + this.height);
+    ctx.lineTo(this.x + radius, this.y + this.height);
+    ctx.quadraticCurveTo(this.x, this.y + this.height - radius, this.x, this.y + this.y - radius);
+    ctx.lineTo(this.x, this.y + radius);
+    ctx.quadraticCurveTo(this.x + radius, this.y, this.x + this.x + radius, this.y);
+
+    if (this.fill) {
+      ctx.fillStyle = this.color;
+      ctx.fill();
+    } else {
+      ctx.strokeStyle = this.color;
+      ctx.stroke();
+    }
+  }
+}
